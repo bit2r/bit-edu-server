89b97e161f45ecf19a18a3d58c9dad3c8791f141
joygram <joygram@vividstudio.co.kr>
2022-10-07 13:00:31 +0900

    bit-server

--- bit-server/run_docker.sh
+++ bit-server/run_docker.sh
@@ -1,1 +1,1 @@
-docker run -d --rm -p 3939:3838 -v "$(pwd)\..\workspace:/home/bit-server/workspace" joygram/bit-server
+docker run -d --rm -p 3939:3838 -p 80:8080 -p 443:8443 -p 22:2222 -v "$(pwd)\..\workspace:/home/bit-server/workspace" joygram/bit-server
