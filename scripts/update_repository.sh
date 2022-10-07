180465c1deb472667500184d373fdc0d03fb10be
joygram <joygram@vividstudio.co.kr>
2022-10-07 14:08:38 +0900

    update script

--- scripts/update_repository.sh
+++ scripts/update_repository.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 if [ ! -d /home/bit-server ]; then
-	echo git clone 
+	echo == git clone == 
 	sudo -u shiny git clone https://github.com/joygram/bit-server.git /home/bit-server
 else 
-	echo git update 
+	echo == git repository update ==
 	pushd .
 	cd /home/bit-server
 	sudo -u shiny git reset --hard
