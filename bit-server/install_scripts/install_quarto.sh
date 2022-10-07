f03d68323bc42c7a7a6732748038ccbd06d3896f
joygram <joygram@vividstudio.co.kr>
2022-10-07 15:12:18 +0900

    update

--- bit-server/install_scripts/install_quarto.sh
+++ bit-server/install_scripts/install_quarto.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
 wget https://quarto.org/download/latest/quarto-linux-amd64.deb
-apt-get install gdebi-core
-gdebi quarto-linux-amd64.deb
+apt-get install gdebi-core -y
+gdebi quarto-linux-amd64.deb -y
