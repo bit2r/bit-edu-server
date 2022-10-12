8c1d9a5f7c3f9f06d62a531cc16c236a92310c09
joygram <joygram@gmail.com>
2022-10-12 15:25:22 +0900

    update

--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -1,24 +1,6 @@
 #!/usr/bin/bash
 
-# update docker's config 
-TARGET_SHINY_CONF=/etc/shiny-server/shiny-server.conf
-TARGET_APACHE_CONF=/etc/apache2
-
-cp ./shiny-server/shiny-server.conf $TARGET_SHINY_CONF
-cp -R ./apache2/* $TARGET_APACHE_CONF
-
-echo == apply custom conf == 
-# apply custom shiny-server config
-CUSTOM_SHINY_CONF=/home/bit-server/workspace/conf/shiny-server/shiny-server.conf
-if [ -f "$CUSTOM_SHINY_CONF" ]; then
-	cp $CUSTOM_SHINY_CONF $TARGET_SHINY_CONF
-fi 
-
-# apply custom apache config 
-CUSTOM_APACHE_CONF=/home/bit-server/workspace/conf/apache2/*
-if [ -d "$TARGET_APACHE_CONF"]; then
-	cp -R $CUSTOM_APACHE_CONF $TARGET_APACHE_CONF
-fi
+souce ./copy_conf.sh
 
 echo == run servers ==
 # run sshd 
