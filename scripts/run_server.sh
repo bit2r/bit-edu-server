fa719033d8e1c4938b96510455eb04acd7df9c0a
joygram <joygram@gmail.com>
2022-10-13 09:34:11 +0900

    update

--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -12,6 +12,4 @@ service apache2 start
 #run mysqld 
 service mysql start 
 
-# sudo -u shiny R -e "options(renv.consent = TRUE);renv::restore(lockfile = '/home/bit-server/workspace/renv.lock')"
-
 exec sudo -u shiny shiny-server
--- scripts/update_repository.sh
+++ scripts/update_repository.sh
@@ -6,7 +6,7 @@ else
 	echo == git repository update ==
 	pushd .
 	cd /home/bit-server
-	sudo -u shiny git reset --hard
+	sudo -u shiny git reset HEAD^ --hard
 	sudo -u shiny git pull 
 	popd 
 fi
