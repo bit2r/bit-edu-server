21eb1f0e46e994d048325ddeda953b53b7da3d42
joygram <joygram@gmail.com>
2022-10-15 14:05:29 +0900

    update

--- docker/run_docker_local.cmd
+++ docker/run_docker_local.cmd
@@ -1,1 +1,1 @@
-docker run -d --rm -p 3939:3838 -p 9090:80 -p 8443:443 -p 3333:22 -v "%cd%\..\workspace:/home/bit-server/workspace" bit-server-local
+docker run -d --rm -p 9090:80 -p 4444:22 -p 3939:3838 -p 8443:443  -v "%cd%\..\workspace:/home/bit-server/workspace" bit-server-local
