1ca33ca9c0486500c19bd36b2923966b6dda2f87
joygram <joygram@gmail.com>
2022-10-17 10:57:54 +0900

    update

--- docker/run_docker_local.sh
+++ docker/run_docker_local.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
-bash ./env.sh
+#!/usr/bin/env bash
+source ./env.sh
 echo start $CONTAINER_NAME : http://localhost:$WEB_PORT
 docker run -d --rm -p $WEB_PORT:80 -p $SSH_PORT:22 -p $SHINY_PORT:3838  -p 8443:443 --name $CONTAINER_NAME -v "$(pwd)/../workspace:/home/bit-server/workspace" bit-server-local
