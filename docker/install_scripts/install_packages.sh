1c8db5ac8f1c74e2a857176a4b5f1ca2f3cd05ea
joygram <joygram@gmail.com>
2022-10-18 08:46:37 +0900

    update

--- docker/install_scripts/install_packages.sh
+++ docker/install_scripts/install_packages.sh
@@ -4,6 +4,7 @@ source ./install_quarto.sh
 # install shinylive and python lib 
 source ./install_shinylive.sh
 
+source ./install_r_packages.sh 
 
 wget https://download3.rstudio.org/ubuntu-18.04/x86_64/shiny-server-1.5.19.995-amd64.deb
 gdebi shiny-server-1.5.19.995-amd64.deb
@@ -11,26 +12,5 @@ gdebi shiny-server-1.5.19.995-amd64.deb
 wget https://download2.rstudio.org/server/jammy/amd64/rstudio-server-2022.07.2-576-amd64.deb
 gdebi rstudio-server-2022.07.2-576-amd64.deb
 
-# install r packages 
-
-install2.r bs4cards clusterSim DALEX DALEXtra EnvStats flexdashboard ggrepel gt
-install2.r janitor keras mixdist modelStudio plotly r2d3 ranger reshape2 rsconnect shinythemes tidymodels
-install2.r KernSmooth MASS Matrix class lattice mgcv nlme gridlayout learnr      
-install2.r DBI DT DiagrammeR R6 RColorBrewer Rcpp Rttf2pt1 askpass assertthat  
-install2.r backports base64enc bit bit64 blob broom      
-install2.r bslib cachem callr cellranger checkmate classInt    
-install2.r cli clipr colourpicker colorspace commonmark cpp11 crayon crosstalk curl        
-install2.r data.table dbplyr digest dlookr downloader dplyr dtplyr digest
-install2.r e1071 ellipsis evaluate extrafont extrafontdb 
-install2.r fansi farver fastmap flextable fontawesome forcats fresh fs 
-install2.r gargle gdtools generics ggplot2 glue googledrive googlesheets gridExtra gtable
-install2.r haven here highr hms htmltools htmlwidgets httpuv httr        
-install2.r ids igraph influenceR isoband jquerylib jsonlite kableExtra knitr labeling later       
-install2.r lazyeval lifecycle lubridate magrittr markdown memoise mime modelr munsell officer openssl openxlsx 
-install2.r pillar pkgconfig prettyunits processx progress promises proxy ps purrr rappdirs readr readxl      
-install2.r rematch rematch2 reprex reticulate rlang rmarkdown rprojroot rstudioapi rvest s2 sass scales selectr     
-install2.r sf shiny shinyAce shinycssload shinydashboa shinybusy shinydashboa shinyWidgets shinyjs showtext showtextdb sourcetools 
-install2.r stringi stringr svglite sys sysfonts systemfonts 
-install2.r tibble tidyr tidyselect tidyverse tinytex 
-install2.r tzdb units utf8 uuid vctrs viridis viridisLite visNetwork vroom waiter webshot withr wk          
-install2.r xfun xml2 xtable yaml zip       
+# for presentation
+npm install reveal.js
--- docker/install_scripts/install_quarto.sh
+++ docker/install_scripts/install_quarto.sh
@@ -4,5 +4,3 @@
 wget https://quarto.org/download/latest/quarto-linux-amd64.deb
 dpkg -i ./quarto-linux-amd64.deb 
 
-# for presentation
-npm install reveal.js
