2c869ff68e012658666e9f6100b153dbdd40e606
joygram <joygram@gmail.com>
2022-10-18 08:40:27 +0900

    update

--- docker/install_scripts/install_packages.sh
+++ docker/install_scripts/install_packages.sh
@@ -4,6 +4,10 @@ source ./install_quarto.sh
 # install shinylive and python lib 
 source ./install_shinylive.sh
 
+
+wget https://download3.rstudio.org/ubuntu-18.04/x86_64/shiny-server-1.5.19.995-amd64.deb
+gdebi shiny-server-1.5.19.995-amd64.deb
+
 wget https://download2.rstudio.org/server/jammy/amd64/rstudio-server-2022.07.2-576-amd64.deb
 gdebi rstudio-server-2022.07.2-576-amd64.deb
 
--- docker/install_scripts/install_quarto.sh
+++ docker/install_scripts/install_quarto.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
+
 wget https://quarto.org/download/latest/quarto-linux-amd64.deb
-apt-get install -y gdebi-core
 dpkg -i ./quarto-linux-amd64.deb 
 
 # for presentation
