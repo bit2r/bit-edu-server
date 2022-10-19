7d4c573fcbd62e09c241a10b160d03d95bcf94e6
joygram <joygram@gmail.com>
2022-10-19 17:26:48 +0900

    update

--- docker/base/run_docker_base.sh
+++ docker/base/run_docker_base.sh
@@ -4,4 +4,4 @@ chmod 755 *.sh
 source ./env.sh
 IMAGE_NAME=bit-server-base
 echo start $IMAGE_NAME to $CONTAINER_NAME : $WEB_PORT : 
-docker run -d --rm -p $WEB_PORT:80 -p $SSH_PORT:22 -p $SHINY_PORT:3838  -p $SSL_PORT:443 --name $CONTAINER_NAME -v "$(pwd)/../workspace:/home/bit-server/workspace" $IMAGE_NAME
+docker run -d -it -p $WEB_PORT:80 -p $SSH_PORT:22 -p $SHINY_PORT:3838  -p $SSL_PORT:443 --name $CONTAINER_NAME -v "$(pwd)/../workspace:/home/bit-server/workspace" $IMAGE_NAME
