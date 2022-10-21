5e6baf6a38490655a33f762f5fee9c89f5427e5d
joygram <joygram@gmail.com>
2022-10-21 21:52:10 +0900

    update env override

--- scripts/workspace_template/env.cmd
+++ scripts/workspace_template/env.cmd
@@ -2,7 +2,4 @@
 rem override for your environment 
 rem set WEB_PORT=9090
 rem set SSH_PORT=4444
-rem set SHINY_PORT=3939
-rem set SSL_PORT=8443
-rem set RSTUDIO_PORT=8787
 rem set CONTAINER_NAME=bit-server
--- scripts/workspace_template/env.sh
+++ scripts/workspace_template/env.sh
@@ -1,19 +1,4 @@
 # override for your environment 
-#WEB_PORT=9090
-#export WEB_PORT
-
-#SSH_PORT=4444
-#export SSH_PORT
-
-#SHINY_PORT=3939
-#export SHINY_PORT
-
-#SSL_PORT=8443
-#export SSL_PORT
-
-#RSTUDIO_PORT=8787
-#export RSTUDIO_PORT
-
-
-#CONTAINER_NAME=bit-server
-#export CONTAINER_NAME
+#export WEB_PORT=9090
+#export SSH_PORT=4444
+#export CONTAINER_NAME=bit-server
