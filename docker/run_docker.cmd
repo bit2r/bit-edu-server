358afe7c19342264653e8f573b9a8d2d184aca87
joygram <joygram@gmail.com>
2022-10-21 21:52:30 +0900

    update run_docker

--- docker/run_docker.cmd
+++ docker/run_docker.cmd
@@ -2,4 +2,4 @@
 set IMAGE_NAME=joygram/bit-server:latest
 echo start %IMAGE_NAME% to %CONTAINER_NAME% : %WEB_PORT%
 rem docker run -d --rm -p %WEB_PORT%:80 -p %SSH_PORT%:22 -p %SHINY_PORT%:3838  -p %SSL_PORT%:443 -p %RSTUDIO_PORT%:8787 --name %CONTAINER_NAME% -v "%cd%\..\workspace:/home/bit-server/workspace" %IMAGE_NAME%
-docker run -d -it -p %WEB_PORT%:80 -p %SSH_PORT%:22 --name %CONTAINER_NAME% -v "%cd%\..\workspace:/home/bit-server/workspace" %IMAGE_NAME%
+docker run -d -it -p %WEB_PORT%:80 -p %SSH_PORT%:22 -p 8080:8080 -p 5000:5000 --name %CONTAINER_NAME% -v "%cd%\..\workspace:/home/bit-server/workspace" %IMAGE_NAME%
