80eb9285994512b443a7379531e87f70b392dc31
joygram <joygram@gmail.com>
2022-11-02 20:54:05 +0900

    update script

--- scripts/update_repository.sh
+++ scripts/update_repository.sh
@@ -19,4 +19,9 @@ if [[ ! -d "$WORKSPACE_PATH/html" ]]; then
 	cp -R $TEMPLATE_PATH $WORKSPACE_PATH
 fi 
 
+# update workspace by joygram 2022/11/02
+if [[ -f "$WORKSPACE_PATH/scripts/update_workspace.sh" ]]; then
+	echo == update workspace == 
+	source $WORKSPACE_PATH/scripts/update_workspace.sh
+fi
 chmod 777 -R /tmp/*
