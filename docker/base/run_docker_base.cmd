54248b29af53ec866f481c7daa5ddcdaaad367a5
joygram <joygram@gmail.com>
2022-11-03 17:11:40 +0900

    update

--- docker/base/run_docker_base.cmd
+++ docker/base/run_docker_base.cmd
@@ -2,4 +2,4 @@ cd ..
 @call .\env.cmd 
 set IMAGE_NAME=bit-server-base
 echo start %IMAGE_NAME% to %CONTAINER_NAME% : %WEB_PORT%
-docker run -d --rm -p %WEB_PORT%:80 -p %SSH_PORT%:22 -p 8787:8787 -p %SHINY_PORT%:3838  -p %SSL_PORT%:443 --name %CONTAINER_NAME% -v "%cd%\..\workspace:/home/bit-server/workspace" %IMAGE_NAME%
+docker run -d -it -p %WEB_PORT%:80 -p %SSH_PORT%:22 -p 8787:8787 -p %SHINY_PORT%:3838  -p %SSL_PORT%:443 --name %CONTAINER_NAME% -v "%cd%\..\workspace:/home/bit-server/workspace" %IMAGE_NAME%
--- docker/install_scripts/install_quarto.sh
+++ docker/install_scripts/install_quarto.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/bash
 
 
-wget https://quarto.org/download/latest/quarto-linux-amd64.deb
-dpkg -i ./quarto-linux-amd64.deb 
-
