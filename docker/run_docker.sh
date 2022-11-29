cc17b81d32d239284551a4eee604129bd9eae247
joygram <joygram@gmail.com>
2022-11-29 13:59:17 +0900

    update run_docker.sh

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 chmod 755 *.sh
 source ./env.sh
-IMAGE_NAME=joygram/bit-server
+IMAGE_NAME=r2bit/bit-edu-server
 echo start $IMAGE_NAME to $CONTAINER_NAME : $WEB_PORT
 docker rm $CONTAINER_NAME
-docker run -d -it --hostname $CONTAINER_NAME -p $WEB_PORT:80 -p $SSH_PORT:22 --name $CONTAINER_NAME -v "$(pwd)/../workspace:/home/bit-server/workspace" $IMAGE_NAME
+docker run -it -d --privileged --hostname $CONTAINER_NAME -p $WEB_PORT:80 -p $SSH_PORT:22 --name $CONTAINER_NAME -v "$(pwd)/../workspace:/home/bit-server/workspace" $IMAGE_NAME
