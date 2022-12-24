d239caa9536c35b9d56883f98ea964e908435ee4
joygram <joygram@gmail.com>
2022-12-24 11:34:33 +0900

    apply php 7.1

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -3,5 +3,6 @@ chmod 755 *.sh
 source ./env.sh
 IMAGE_NAME=r2bit/bit-edu-server
 echo start $IMAGE_NAME to $CONTAINER_NAME : $WEB_PORT
+docker pull $IMAGE_NAME
 docker rm $CONTAINER_NAME
 docker run -it -d --privileged --hostname $CONTAINER_NAME -p $WEB_PORT:80 -p $SSH_PORT:22 --name $CONTAINER_NAME -v "$(pwd)/../workspace:/home/bit-server/workspace" $IMAGE_NAME
